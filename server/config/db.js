b092453a925fd54b2b29b0bd1412ff269f6c164a
Daniel Knights <danknights95@gmail.com>
2020-09-25 10:54:15 +0100

    Minor updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,10 @@
-const mongodb = require("mongodb");
+const mongodb = require('mongodb');
 
 const connectDb = mongodb.MongoClient.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 }).catch(err => console.error(err));
+
+if (connectDb) console.log('MongoDB Connected');
 
 module.exports = connectDb;
