1edc021896cff9fe4d872a2a1abbe45fd94a51ca
Daniel Knights <danknights95@gmail.com>
2020-09-28 17:52:03 +0100

    Removed server/public from .gitignore

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,48 +1,43 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-import VueMeta from "vue-meta";
+import VueMeta from 'vue-meta';
 
 Vue.use(VueRouter);
 Vue.use(VueMeta);
 
 const routes = [
     {
-        path: "/",
-        name: "index",
-        component: () =>
-            import(/* webpackChunkName: "index" */ "../views/index.vue")
+        path: '/',
+        name: 'index',
+        component: () => import(/* webpackChunkName: "index" */ '../views/index.vue'),
     },
     {
-        path: "/login",
-        name: "login",
-        component: () =>
-            import(/* webpackChunkName: "login" */ "../views/login.vue")
+        path: '/login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "login" */ '../views/login.vue'),
     },
     {
-        path: "/signup",
-        name: "signup",
-        component: () =>
-            import(/* webpackChunkName: "signup" */ "../views/signup.vue")
+        path: '/signup',
+        name: 'signup',
+        component: () => import(/* webpackChunkName: "signup" */ '../views/signup.vue'),
     },
     {
-        path: "/photos",
-        name: "photos",
-        component: () =>
-            import(/* webpackChunkName: "photos" */ "../views/photos.vue")
+        path: '/photos',
+        name: 'photos',
+        component: () => import(/* webpackChunkName: "photos" */ '../views/photos.vue'),
     },
     {
-        path: "/contact",
-        name: "contact",
-        component: () =>
-            import(/* webpackChunkName: "contact" */ "../views/contact.vue")
-    }
+        path: '/contact',
+        name: 'contact',
+        component: () => import(/* webpackChunkName: "contact" */ '../views/contact.vue'),
+    },
 ];
 
 const router = new VueRouter({
-    mode: "history",
+    mode: 'history',
     base: process.env.BASE_URL,
-    routes
+    routes,
 });
 
 export default router;
